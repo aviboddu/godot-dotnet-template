c457c90dc934e822a44e713bcc55016984cd4f34
aviboddu <aviboddu@gmail.com>
2023-12-02 18:27:20 -0800

    Added basic change setting method

--- utilities/settings/Configuration.cs
+++ utilities/settings/Configuration.cs
@@ -8,28 +8,44 @@ public abstract partial class Configuration : Node
 	private readonly string configFilePath = "./config.ini";
 	private ConfigFile configFile;
 
-	private Logger logger;
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		logger = Logger.Instance;
 		configFile = new();
 		if (File.Exists(configFilePath))
 		{
 			if (configFile.Load(configFilePath) != Error.Ok)
 			{
-				logger.WriteWarning("Configuration::_Ready() - Failed to load config file");
+				Logger.Instance.WriteWarning("Configuration::_Ready() - Failed to load config file");
 				LoadDefaultConfig();
-				configFile.Save(configFilePath);
+				Save();
+			}
+			else
+			{
+				Logger.Instance.WriteInfo("Configuration::_Ready() - Loaded config file");
 			}
 		}
 		else
 		{
 			LoadDefaultConfig();
-			configFile.Save(configFilePath);
+			Save();
 		}
 	}
 
 	protected abstract void LoadDefaultConfig();
+
+	protected void ChangeSetting(string section, string key, string value)
+	{
+		configFile.SetValue(section, key, value);
+		Logger.Instance.WriteInfo($"Configuration::ChangeSetting() - Changed {section}:{key} to {value}");
+		CallDeferred(MethodName.Save);
+	}
+
+	private void Save()
+	{
+		if (configFile.Save(configFilePath) != Error.Ok)
+			Logger.Instance.WriteWarning("Configuration::Save() - Failed to save config");
+		else
+			Logger.Instance.WriteDebug("Configuration::Save() - Saved config");
+	}
 }
