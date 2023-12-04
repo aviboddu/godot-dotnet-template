fc08514e8f87bd28f95f0910e6e0fa506d056137
aviboddu <aviboddu@gmail.com>
2023-12-04 11:58:45 -0800

    Created AudioManager

---
+++ utilities/managers/AudioManager.cs
@@ -0,0 +1,60 @@
+using Godot;
+
+namespace Utilities;
+
+// TODO: Add helper functions to play audio files.
+public partial class AudioManager : Node
+{
+
+	private const string AUDIO_SECTION = "Audio";
+
+	private float _masterVolume;
+	public float MasterVolume
+	{
+		get => _masterVolume;
+		set
+		{
+			_masterVolume = value;
+			Configuration.Instance.ChangeSetting(AUDIO_SECTION, PropertyName.MasterVolume, value);
+		}
+	}
+
+	private float _musicVolume;
+	public float MusicVolume
+	{
+		get => _musicVolume;
+		set
+		{
+			_musicVolume = value;
+			Configuration.Instance.ChangeSetting(AUDIO_SECTION, PropertyName.MusicVolume, value);
+		}
+	}
+
+	private float _sfxVolume;
+	public float SfxVolume
+	{
+		get => _sfxVolume;
+		set
+		{
+			_sfxVolume = value;
+			Configuration.Instance.ChangeSetting(AUDIO_SECTION, PropertyName.SfxVolume, value);
+		}
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		if (Configuration.Instance.HasSetting(AUDIO_SECTION, PropertyName.MasterVolume))
+		{
+			_masterVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.MasterVolume);
+			_musicVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.MusicVolume);
+			_sfxVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.SfxVolume);
+		}
+		else
+		{
+			MasterVolume = 1F;
+			MusicVolume = 1F;
+			SfxVolume = 1F;
+		}
+	}
+}
--- utilities/settings/Configuration.cs
+++ utilities/settings/Configuration.cs
@@ -8,32 +8,41 @@ public abstract partial class Configuration : Node
 	private const string CONFIG_FILE_PATH = "./config.ini";
 	private readonly ConfigFile configFile = new();
 
+	public static Configuration Instance { get; private set; }
+	public override void _EnterTree()
+	{
+		if (Instance != null)
+			QueueFree(); // The singleton is already loaded, kill this instance
+		Instance = this;
+	}
+
 	public override void _Ready()
 	{
 		if (File.Exists(CONFIG_FILE_PATH))
 		{
 			if (configFile.Load(CONFIG_FILE_PATH) != Error.Ok)
-			{
 				Logger.Instance.WriteWarning("Configuration::_Ready() - Failed to load config file");
-				LoadDefaultConfig();
-				Save();
-			}
 			else
-			{
 				Logger.Instance.WriteInfo("Configuration::_Ready() - Loaded config file");
-			}
 		}
 		else
 		{
 			Logger.Instance.WriteInfo("Configuration::_Ready() - No config file found");
-			LoadDefaultConfig();
-			Save();
 		}
 	}
 
-	protected abstract void LoadDefaultConfig();
+	public T GetSetting<[MustBeVariant] T>(string section, string key)
+	{
+		return configFile.GetValue(section, key).As<T>();
+	}
 
-	protected void ChangeSetting(string section, string key, Variant value)
+
+	public bool HasSetting(string section, string key)
+	{
+		return configFile.HasSectionKey(section, key);
+	}
+
+	public void ChangeSetting(string section, string key, Variant value)
 	{
 		configFile.SetValue(section, key, value);
 		Logger.Instance.WriteInfo($"Configuration::ChangeSetting() - Changed {section}:{key} to {value}");
