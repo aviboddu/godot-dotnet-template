7587cf3d6111eed8a44b268e3fc86f2114335bae
aviboddu <aviboddu@gmail.com>
2023-12-06 00:42:47 -0800

    Added AudioBus integration to AudioManager

--- utilities/managers/AudioManager.cs
+++ utilities/managers/AudioManager.cs
@@ -1,42 +1,55 @@
+using System.Diagnostics;
 using Godot;
 
 namespace Utilities;
 
-// TODO: Add helper functions to play audio files.
 public partial class AudioManager : Node
 {
 
 	private const string AUDIO_SECTION = "Audio";
+	private const string MASTER_BUS_NAME = "Master";
+	private const string MUSIC_BUS_NAME = "Music";
+	private const string SFX_BUS_NAME = "SFX";
 
-	private float _masterVolume;
+	private readonly int _masterBusIndex = AudioServer.GetBusIndex(MASTER_BUS_NAME);
+	private readonly int _musicBusIndex = AudioServer.GetBusIndex(MUSIC_BUS_NAME);
+	private readonly int _sfxBusIndex = AudioServer.GetBusIndex(SFX_BUS_NAME);
+
+	private float _masterVolume = float.NaN;
 	public float MasterVolume
 	{
 		get => _masterVolume;
 		set
 		{
+			if (_masterVolume == value) return;
 			_masterVolume = value;
+			SetBusVolume(_masterBusIndex, value);
 			Configuration.Instance.ChangeSetting(AUDIO_SECTION, PropertyName.MasterVolume, value);
 		}
 	}
 
-	private float _musicVolume;
+	private float _musicVolume = float.NaN;
 	public float MusicVolume
 	{
 		get => _musicVolume;
 		set
 		{
+			if (_musicVolume == value) return;
 			_musicVolume = value;
+			SetBusVolume(_musicBusIndex, value);
 			Configuration.Instance.ChangeSetting(AUDIO_SECTION, PropertyName.MusicVolume, value);
 		}
 	}
 
-	private float _sfxVolume;
+	private float _sfxVolume = float.NaN;
 	public float SfxVolume
 	{
 		get => _sfxVolume;
 		set
 		{
+			if (_sfxVolume == value) return;
 			_sfxVolume = value;
+			SetBusVolume(_sfxBusIndex, value);
 			Configuration.Instance.ChangeSetting(AUDIO_SECTION, PropertyName.SfxVolume, value);
 		}
 	}
@@ -50,22 +63,47 @@ public partial class AudioManager : Node
 		Instance = this;
 	}
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
 		if (Configuration.Instance.HasSection(AUDIO_SECTION))
 		{
 			Logger.Instance.WriteInfo("AudioManager::_Ready() - Initializing Volumes from Configuration");
-			_masterVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.MasterVolume);
-			_musicVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.MusicVolume);
-			_sfxVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.SfxVolume);
+			MasterVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.MasterVolume);
+			MusicVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.MusicVolume);
+			SfxVolume = Configuration.Instance.GetSetting<float>(AUDIO_SECTION, PropertyName.SfxVolume);
 		}
 		else
 		{
 			Logger.Instance.WriteInfo("AudioManager::_Ready() - Default Initialization");
-			MasterVolume = 1F;
-			MusicVolume = 1F;
-			SfxVolume = 1F;
+			MasterVolume = GetBusVolume(_masterBusIndex);
+			MusicVolume = GetBusVolume(_musicBusIndex);
+			SfxVolume = GetBusVolume(_sfxBusIndex);
 		}
+	}
+
+	private static void SetBusVolume(int busIndex, float volume)
+	{
+		bool isZero = volume == 0;
+		AudioServer.SetBusMute(busIndex, isZero); // Set Mute Correctly
+		if (!isZero) AudioServer.SetBusVolumeDb(busIndex, ConvertToDecibels(volume)); // Adjust Decibels accordingly
+	}
+
+	private static float GetBusVolume(int busIndex)
+	{
+		if (AudioServer.IsBusMute(busIndex))
+			return 0;
+		return ConvertFromDecibels(AudioServer.GetBusVolumeDb(busIndex));
+	}
+
+	private static float ConvertToDecibels(float x)
+	{
+		Debug.Assert(x > 0, $"AudioManager::ConvertToDecibels({x}) - {x} must be greater than 0");
+		return 20 * (float)System.Math.Log10(x);
+	}
+
+	private static float ConvertFromDecibels(float db)
+	{
+		return (float)System.Math.Pow(10, db / 20);
 	}
 }
