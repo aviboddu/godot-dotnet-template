6d5be2806293946288c85211ff8053b162b43d8f
aviboddu <aviboddu@gmail.com>
2023-12-06 17:47:58 -0800

    Added basic menu menu framework

--- MainMenu.cs
+++ MainMenu.cs
@@ -3,6 +3,26 @@ using Utilities;
 
 public partial class MainMenu : Control
 {
+	[Export]
+	Control Settings;
+
+	[Export]
+	Control StartMenu;
+
+	public void _on_settings_pressed()
+	{
+		Logger.Instance.WriteInfo("User Opened Settings");
+		Settings.Visible = true;
+		StartMenu.Visible = false;
+	}
+
+	public void _on_back_pressed()
+	{
+		Logger.Instance.WriteInfo("User Exited Settings");
+		Settings.Visible = false;
+		StartMenu.Visible = true;
+	}
+
 	public void _on_quit_pressed()
 	{
 		Logger.Instance.WriteInfo("User Quit");
