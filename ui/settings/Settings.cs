0725c12b296d413b7f6830551194db737c7d16a1
aviboddu <aviboddu@gmail.com>
2023-12-17 22:08:37 -0800

    Simplified some UI layouts

--- ui/settings/Settings.cs
+++ ui/settings/Settings.cs
@@ -1,13 +1,10 @@
 using Godot;
 
 namespace UI;
-public partial class Settings : VBoxContainer
+public partial class Settings : Control
 {
 	[Signal]
 	public delegate void BackPressedEventHandler();
 
-	public void _on_back_pressed()
-	{
-		EmitSignal(SignalName.BackPressed);
-	}
+	public void _on_back_pressed() => EmitSignal(SignalName.BackPressed);
 }
